cded490d4b6d861483cbcd5fefe7ed6d98d06a78
Matthew Richards <matthew.richards@softwire.com>
2017-12-03 15:57:29 +0000

    Day 3, part 1 - I'm not sure whether "just write some code" is liberating, or unfortunate...

--- ADventOfCode2017/Program.cs
+++ ADventOfCode2017/Program.cs
@@ -10,10 +10,91 @@ namespace AdventOfCode2017
     {
         static void Main(string[] args)
         {
-            Day2Part2();
+            Day3Part1();
 
             Console.In.ReadLine();
         }
+
+        enum Direction
+        {
+            Right,
+            Left,
+            Up,
+            Down
+        }
+
+        private static void Day3Part1()
+        {
+            int targetSquare = 361527;
+
+            int currentSquare = 1;
+            int howRight = 0;
+            int howUp = 0;
+            int spiralSize = 1;
+            int howSpiralled = 0;
+            Direction direction = Direction.Right;
+             
+            while (currentSquare < targetSquare)
+            {
+                currentSquare++;
+
+                switch (direction)
+                {
+                    case Direction.Right:
+                        howRight++;
+                        howSpiralled++;
+
+                        if (howSpiralled >= spiralSize)
+                        {
+                            direction = Direction.Up;
+                            howSpiralled = 0;
+                        }
+                        break;
+
+                    case Direction.Up:
+                        howUp++;
+                        howSpiralled++;
+
+                        if (howSpiralled >= spiralSize)
+                        {
+                            direction = Direction.Left;
+                            howSpiralled = 0;
+                            spiralSize++;
+                        }
+                        break;
+
+                    case Direction.Left:
+                        howRight--;
+                        howSpiralled++;
+
+                        if (howSpiralled >= spiralSize)
+                        {
+                            direction = Direction.Down;
+                            howSpiralled = 0;
+                        }
+                        break;
+
+                    case Direction.Down:
+                        howUp--;
+                        howSpiralled++;
+
+                        if (howSpiralled >= spiralSize)
+                        {
+                            direction = Direction.Right;
+                            howSpiralled = 0;
+                            spiralSize++;
+                        }
+
+                        break;
+                        }
+
+                }
+
+            int answer = Math.Abs(howRight) + Math.Abs(howUp);
+            Console.WriteLine($"Answer is {answer} because we've gone right {howRight} and up {howUp}");
+
+            }
+        
 
         static void Day1Part1()
 
