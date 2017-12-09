9800ba80a7a4fa3ea170ae269df23f3f36902fdb
Matthew Richards <matthew.richards@softwire.com>
2017-12-09 20:45:36 +0000

    Day 8, part 2

--- ADventOfCode2017/Program.cs
+++ ADventOfCode2017/Program.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode2017
     {
         static void Main(string[] args)
         {
-            var solver = new Day8Part1();
+            var solver = new Day8Part2();
 
             int answer = solver.Solve();
 
