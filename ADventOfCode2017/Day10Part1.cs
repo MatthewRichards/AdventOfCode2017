6afaaecf04b99dad521416bcdd59c768836e93e4
Matthew Richards <matthew.richards@softwire.com>
2017-12-10 20:34:36 +0000

    Day 10, part 1

---
+++ ADventOfCode2017/Day10Part1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2017
+{
+    public class Day10Part1 : ISolution
+    {
+        public int Solve()
+        {
+            var list = Enumerable.Range(0, 256).ToArray();
+            int currentPosition = 0;
+            int skipSize = 0;
+            var lengths = Input.Split(',').Select(int.Parse);
+
+            foreach (var length in lengths)
+            {
+                Reverse(list, currentPosition, length);
+
+                currentPosition += length + skipSize;
+                skipSize++;
+                while (currentPosition >= list.Length) currentPosition -= list.Length;
+
+            }
+
+            return list[0] * list[1];
+
+        }
+
+        private void Reverse(int[] list, int start, int length)
+        {
+            for(int i=0;i<length/2;i++)
+            {
+                var oneEnd = start + i;
+                while (oneEnd >= list.Length) oneEnd -= list.Length;
+                var otherEnd = start + length-1-i;
+                while (otherEnd >= list.Length) otherEnd -= list.Length;
+
+                var swap = list[oneEnd];
+                list[oneEnd] = list[otherEnd];
+                list[otherEnd] = swap;
+            }
+        }
+
+        private static string TestInput = @"3, 4, 1, 5";
+        private static string Input = @"206,63,255,131,65,80,238,157,254,24,133,2,16,0,1,3";
+    }
+}
--- ADventOfCode2017/Program.cs
+++ ADventOfCode2017/Program.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode2017
     {
         static void Main(string[] args)
         {
-            var solver = new Day9Part2();
+            var solver = new Day10Part1();
 
             int answer = solver.Solve();
 
