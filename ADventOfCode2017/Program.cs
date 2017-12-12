7f168a02a4ca25f379c92602b4619f78cae69a50
Matthew Richards <matthew.richards@softwire.com>
2017-12-12 21:46:00 +0000

    Day 12, part 1 - could do better

--- ADventOfCode2017/Program.cs
+++ ADventOfCode2017/Program.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode2017
     {
         static void Main(string[] args)
         {
-            var solver = new Day11Part2();
+            var solver = new Day12Part1();
 
             int answer = solver.Solve();
 
