66a2b01fbd4e6525f3e19d9ab3773bba32e67a92
Matthew Richards <Matthew.Richards@softwire.com>
2017-12-19 13:08:33 +0000

    Day 17 part 1, which seems ok provided I make a dedicated data structure rather than trying to use anything built-in

---
+++ ADventOfCode2017/Day17Part1.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+
+namespace AdventOfCode2017
+{
+  public class Day17Part1 : ISolution
+  {
+    public int Solve()
+    {
+      var buffer = new CircularBuffer();
+      int stepSize = 370;
+
+      for (int numberToInsert = 1; numberToInsert <= 2017; numberToInsert++)
+      {
+        for (int i = 0; i < stepSize; i++)
+          {
+            // Step forward...
+
+            buffer.StepForward();
+          }
+
+        // Insert the next value, and use as the current position
+        buffer.CurrentPosition = buffer.CurrentPosition.InsertAfter(numberToInsert);
+
+      }
+
+      return buffer.CurrentPosition.Next.Value;
+
+    }
+
+  }
+
+  public class CircularBuffer
+  {
+
+    public BufferNode CurrentPosition { get; set; }
+    public CircularBuffer()
+    {
+      CurrentPosition = new BufferNode() {Value = 0};
+      CurrentPosition.Next = CurrentPosition;
+    }
+
+    public void StepForward()
+    {
+      CurrentPosition = CurrentPosition.Next;
+    }
+
+    public class BufferNode
+    {
+      public int Value { get; set; }
+      public BufferNode Next { get; set; }
+
+      public BufferNode InsertAfter(int newValue)
+      {
+        var newNode = new BufferNode {Value = newValue, Next = Next};
+        this.Next = newNode;
+        return newNode;
+
+      }
+    }
+  }
+}
--- ADventOfCode2017/Program.cs
+++ ADventOfCode2017/Program.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode2017
     {
         static void Main(string[] args)
         {
-            var solver = new Day16Part2();
+            var solver = new Day17Part1();
 
             int answer = solver.Solve();
 
