1870121092d9d637cc7ada075198fd47d733b66d
Matthew Richards <Matthew.Richards@softwire.com>
2017-12-20 22:23:50 +0000

    Day 19 part 1, in which slow, steady and horribly duplicated wins the race, although leaves me a little puzzled about when the infinite loop is going to end...

--- ADventOfCode2017/Program.cs
+++ ADventOfCode2017/Program.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode2017
     {
         static void Main(string[] args)
         {
-            var solver = new Day18Part2();
+            var solver = new Day19Part1();
 
             int answer = solver.Solve();
 
